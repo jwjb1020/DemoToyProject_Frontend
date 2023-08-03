61433b91e1a2204968b6ef389422343428dda23f
jwjb1020 <jwjb1020@gmail.com>
2023-08-04 01:50:57 +0900

    2023.08.04

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Athentication from './views/Athentication';
 import MainPage from './pages/mainpage/mainpage';
+import SignInAddSignUp from './views/Athentication/SignInAndSignUp';
 
 
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
   return (
     <>
       {/* <Athentication /> */}
-     <MainPage/>
+     {/* <MainPage/> */}
+     <SignInAddSignUp/>
     </>
 
 
